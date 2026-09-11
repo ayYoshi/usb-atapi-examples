8e9090feb99799bf588a1977e4ce18786e0c303d
Ayush Joshi <ayushjoshi754@gmail.com>
2026-09-11 15:51:32 -0700

    created readcd.c example in which the program reads 75 frames of CD-DA data

--- examples/eject.c
+++ examples/eject.c
@@ -41,7 +41,7 @@ int main(int argc, char *argv[]) {
   }
   printf("sending start/stop unit command (disc ejection)...\n");
   // to eject a disc, LoEj must be set to 1 and START must be set to 0
-  rc = scsi_start_stop_unit(discreader, 0, 1, 0);
+  rc = scsi_start_stop_unit(discreader, 0, 0, 1, 0);
   if (rc != 0) {
     printf("command failed with %d\n. getting sense...", rc);
     struct scsi_sense_data sense;
